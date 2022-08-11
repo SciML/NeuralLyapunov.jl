57b81c9175f0752345204def89172991ec1787c6
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2022-08-11 13:41:06 -0400

    Made strictly positive harmonic function w/o bc

--- harmonic_nobc.jl
+++ harmonic_nobc.jl
@@ -13,12 +13,13 @@ grad(f) = Symbolics.gradient(f, [x, y])
 
 # Define PDE system
 us = [u1(x,y), u2(x,y)]
-u(x0,y0) = (u1(x0,y0))^2 + (u2(x0,y0))^2
-eq = divergence(grad(u(x,y))) ~ 0
+δ = 0.01
+V(x0,y0) = (u1(x0,y0))^2 + (u2(x0,y0))^2 + δ*log(1. + x0^2 + y0^2)
+eq = divergence(grad(V(x,y))) ~ 0
 domains = [ x ∈ (0.0, pi),
             y ∈ (0.0, 1.0) 
             ]
-bcs = [ u(0.5, 0.5) ~ 1. ]
+bcs = [ V(0.5, 0.5) ~ 1. ]
 
 @named pde_system = PDESystem(eq, bcs, domains, [x, y], us)
 
@@ -36,7 +37,7 @@ chain = [Lux.Chain(
 #TODO: Only QuasiRandomTraining works
 #strategy = QuadratureTraining()
 #strategy = GridTraining(0.05)
-strategy = QuasiRandomTraining(100)
+strategy = QuasiRandomTraining(500)
 discretization = PhysicsInformedNN(chain, strategy)
 prob = discretize(pde_system, discretization)
 
@@ -50,11 +51,11 @@ res = Optimization.solve(prob, BFGS(); callback=callback, maxiters=5000)
 phi = discretization.phi
 minimizers_ = [res.u.depvar[Symbol(:u,i)] for i in 1:length(us)]
 us_predict_func(x0,y0) = [ phi[i]([x0,y0],minimizers_[i])[1] for i in 1:length(us) ]
-#=function predicted_sol_func(x0,y0) 
-    u_vec = u_predict_func(x0,y0)
-    sin(u_vec[1]) * exp(u_vec[2])
-end=#
-predicted_sol_func = ( (s) -> s[1]^2 + s[2]^2 ) ∘ us_predict_func
+function predicted_sol_func(x0,y0) 
+    u_vec = us_predict_func(x0,y0)
+    u_vec[1]^2 + u_vec[2]^2 + δ*log(1 + x0^2 + y0^2)
+end
+#predicted_sol_func = ( (s) -> s[1]^2 + s[2]^2 ) ∘ us_predict_func
 
 # Plot results
 xs,ys = [ModelingToolkit.infimum(d.domain):0.01:ModelingToolkit.supremum(d.domain) for d in domains]
@@ -85,6 +86,10 @@ p2 = plot(xs, ys, lap_predict, linetype=:contourf, title="laplacian");
 #p4 = plot(xs, ys, curl_predict, linetype=:contourf, title="curl of grad");
 plot(p1, p2)#, p3, p4)
 savefig("harmonic_sol_nobc")
+
+println("V(0.5,0.5) = ", predicted_sol_func(0.5,0.5))
+println("V ∈ (", minimum(u_predict), ", ", maximum(u_predict), ")")
+println("ΔV ∈ (", minimum(lap_predict), ", ", maximum(lap_predict), ")")
 
 #V_fun(x,v) = norm(φ_fun(x,v) - φ_fun(0.,0.))^2 + δ * log(1.0 + x^2 + v^2) 
 #dVdt_fun(x0,v0) = Symbolics.value.(substitute(f, Dict([x=>x0, v=>v0]))) ⋅ gradient(V_fun, x0, v0)
