847feef6a4d8a639878f33c0de4b1cee6cf0df3c
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2023-05-29 22:50:24 -0400

    Region of attraction no longer exported

--- src/NeuralLyapunov.jl
+++ src/NeuralLyapunov.jl
@@ -9,12 +9,11 @@ import Hypatia, JuMP
 
 include("conditions_specification.jl")
 include("NeuralLyapunovPDESystem.jl")
-include("RoAEstimation.jl")
 include("local_Lyapunov.jl")
 
 
 export NeuralLyapunovPDESystem, NumericalNeuralLyapunovFunctions
-export get_RoA_estimate, local_Lyapunov
+export local_Lyapunov
 export NeuralLyapunovSpecification, NeuralLyapunovStructure, 
     UnstructuredNeuralLyapunov, NonnegativeNeuralLyapunov, 
     LyapunovMinimizationCondition, StrictlyPositiveDefinite, 
