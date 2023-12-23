ed7eb4dfebdf609db4f52f886561e2a1a612be38
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2023-12-23 15:46:30 -0500

    Added experiment with approx dynamics and no data

--- demos/incorporate_data/single_dimension.jl
+++ demos/incorporate_data/single_dimension.jl
@@ -30,13 +30,9 @@ chain = [
 
 # Define neural network discretization
 strategy = GridTraining(0.1)
-discretization = PhysicsInformedNN(chain, strategy)
 
 # Define neural Lyapunov structure
-structure = NonnegativeNeuralLyapunov(
-        dim_output; 
-        δ = 1e-6
-        )
+structure = PositiveSemiDefiniteStructure(dim_output)
 minimization_condition = DontCheckNonnegativity(check_fixed_point = true)
 
 # Define Lyapunov decrease condition
@@ -44,193 +40,185 @@ decrease_condition = AsymptoticDecrease(strict = true)
 
 # Construct neural Lyapunov specification
 spec = NeuralLyapunovSpecification(
-    structure,
-    minimization_condition,
-    decrease_condition,
+        structure,
+        minimization_condition,
+        decrease_condition,
     )
-
-############################# Construct PDESystem #############################
-
-pde_system, network_func = NeuralLyapunovPDESystem(
-    true_dynamics,
-    lb,
-    ub,
-    spec
-)
-
-######################## Construct OptimizationProblem ########################
-
-prob = discretize(pde_system, discretization)
-sym_prob = symbolic_discretize(pde_system, discretization)
 
 callback = function (p, l)
     println("loss: ", l)
     return false
 end
 
-########################## Solve OptimizationProblem ##########################
-
-res = Optimization.solve(prob, Adam(); callback = callback, maxiters = 300)
-
-prob = Optimization.remake(prob, u0 = res.u);
-res = Optimization.solve(prob, Adam(); callback = callback, maxiters = 300)
-
-println("Switching from Adam to BFGS");
-prob = Optimization.remake(prob, u0 = res.u);
-res = Optimization.solve(prob, BFGS(); callback = callback, maxiters = 300)
-
-###################### Get numerical numerical functions ######################
-V_func, V̇_func, ∇V_func = NumericalNeuralLyapunovFunctions(
-    discretization.phi, 
-    res, 
-    network_func, 
-    structure.V,
-    true_dynamics,
-    zeros(length(lb))
-    )
-
-################################## Simulate ###################################
-xs = lb[1]:0.02:ub[1] 
-V_predict = [V_func([x0]) for x0 in xs]
-dVdt_predict  = [V̇_func([x0]) for x0 in xs]
-
-# Print statistics
-println("V(0.,0.) = ", V_func([0.0]))
-println("dVdt(0.,0.) = ", V̇_func([0.0]))
-println("V ∋ [", min(V_func([0.0]), minimum(V_predict)), ", ", maximum(V_predict), "]")
-println(
-    "V̇ ∋ [",
-    minimum(dVdt_predict),
-    ", ",
-    max(V̇_func([0.0]), maximum(dVdt_predict)),
-    "]",
-)
-
-# Get RoA Estimate
-invalid_region = xs[dVdt_predict .>= 0]
-invalid_start = maximum(invalid_region[invalid_region .< 0])
-invalid_end = minimum(invalid_region[invalid_region .> 0])
-valid_region = xs[invalid_start .< xs .< invalid_end]
-ρ = min(V_func([first(valid_region)]), V_func([last(valid_region)]))
-RoA = valid_region[vec(V_func(transpose(valid_region))) .≤ ρ]
-
-println("Estimated region of attraction: [$(first(RoA)), $(last(RoA))]")
-println("True region of attraction: (-1, 1)")
-
-# Plot results
-p1 = plot(xs, V_predict, label = "V", xlabel = "x", linewidth=2);
-p1 = hline!([ρ], label = "V = $(round(ρ, digits = 4))", legend = :inside)
-p1 = vspan!([first(RoA), last(RoA)]; label = "Estimated Region of Attraction", opacity = 0.2, color = :green);
-p1 = vspan!([-1, 1]; label = "True Region of Attraction", color = :gray, fillstyle = :/);
-
-p2 = plot(xs, dVdt_predict, label = "dV/dt", xlabel = "x", linewidth=2);
-p2 = hline!([0.0], label = "dV/dt = 0", legend = :bottom)
-p2 = vspan!([first(RoA), last(RoA)]; label = "Estimated Region of Attraction", opacity = 0.2, color = :green);
-p2 = vspan!([-1, 1]; label = "True Region of Attraction", color = :gray, fillstyle = :/);
-
-plot(p1, p2)
-
 ########################### Generate training data ############################
-xs = (ub[1] - lb[1]) * rand(20) .+ lb[1]
-data = [([x], f_true(x, [], 0.0)) for x in xs]
+data_xs = (ub[1] - lb[1]) * rand(20) .+ lb[1]
+data = [([x], f_true(x, [], 0.0)) for x in data_xs]
+
+simulation_xs = lb[1]:0.02:ub[1] 
 
 ######################### Define approximate dynamics #########################
-Random.seed!(200)
 f_approx(x, p, t) = -x
 approx_dynamics = ODEFunction(f_approx; syms = [:x])
 
-############################# Construct PDESystem #############################
+########################### Loop through experiments ##########################
 
-pde_system, network_func = NeuralLyapunovPDESystem(
-    approx_dynamics,
-    lb,
-    ub,
-    spec
-)
+struct LyapunovResults
+    θ
+    V::Function
+    V̇::Function
+    ρ::Real
+    RoA::Tuple{Real, Real}
+    plt
+end
 
-####################### Specify neural Lyapunov problem #######################
-# Specify neural net
-dim_state = length(lb)
-dim_hidden = 15
-dim_output = 2
-chain = [
-    Lux.Chain(
-        Dense(dim_state, dim_hidden, tanh),
-        Dense(dim_hidden, dim_hidden, tanh),
-        Dense(dim_hidden, 1, use_bias = false),
-    ) for _ = 1:dim_output
-]
+results = Dict{Symbol, LyapunovResults}()
 
-# Generate addtional loss
-data_loss = additional_loss_from_data(data, spec, network_func; fixed_point = [0.0])
+for experiment in [:true_dynamics, :approx_no_data, :approx_with_data]
+    Random.seed!(200)
+    println("\nBeginning experiment ", experiment)
 
-# Define neural network discretization
-strategy = GridTraining(0.1)
-discretization = PhysicsInformedNN(chain, strategy, additional_loss = data_loss)
+    ######################## Construct PDESystem ##########################
 
-######################## Construct OptimizationProblem ########################
+    pde_system, network_func = if experiment == :true_dynamics
+        NeuralLyapunovPDESystem(
+            true_dynamics,
+            lb,
+            ub,
+            spec
+        )
+    else
+        NeuralLyapunovPDESystem(
+            approx_dynamics,
+            lb,
+            ub,
+            spec
+        )
+    end
 
-prob = discretize(pde_system, discretization)
-sym_prob = symbolic_discretize(pde_system, discretization)
+    ####################### Generate discretization #######################
 
-########################## Solve OptimizationProblem ##########################
+    discretization = if experiment == :approx_with_data
+        # Generate addtional loss
+        data_loss = additional_loss_from_data(
+            data, 
+            spec, 
+            network_func; 
+            fixed_point = [0.0]
+        )
+        PhysicsInformedNN(chain, strategy, additional_loss = data_loss)
+    else
+        PhysicsInformedNN(chain, strategy)
+    end
 
-res = Optimization.solve(prob, Adam(); callback = callback, maxiters = 300)
+    #################### Construct OptimizationProblem ####################
 
-prob = Optimization.remake(prob, u0 = res.u);
-res = Optimization.solve(prob, Adam(); callback = callback, maxiters = 300)
+    prob = discretize(pde_system, discretization)
+    sym_prob = symbolic_discretize(pde_system, discretization)
 
-println("Switching from Adam to BFGS");
-prob = Optimization.remake(prob, u0 = res.u);
-res = Optimization.solve(prob, BFGS(); callback = callback, maxiters = 300)
+    ###################### Solve OptimizationProblem ######################
 
-###################### Get numerical numerical functions ######################
-V_func, V̇_func, ∇V_func = NumericalNeuralLyapunovFunctions(
-    discretization.phi, 
-    res, 
-    network_func, 
-    structure.V,
-    true_dynamics,
-    zeros(length(lb))
+    res = Optimization.solve(prob, Adam(); callback = callback, maxiters = 300)
+
+    prob = Optimization.remake(prob, u0 = res.u);
+    res = Optimization.solve(prob, Adam(); callback = callback, maxiters = 300)
+
+    println("Switching from Adam to BFGS");
+    prob = Optimization.remake(prob, u0 = res.u);
+    res = Optimization.solve(prob, BFGS(); callback = callback, maxiters = 300)
+
+    ################## Get numerical numerical functions ##################
+    V_func, V̇_func, _ = NumericalNeuralLyapunovFunctions(
+            discretization.phi, 
+            res.u, 
+            network_func, 
+            structure.V,
+            true_dynamics,
+            zeros(length(lb))
+        )
+
+    ############################# Save results ############################
+
+    results[experiment] = LyapunovResults(
+        res.u, 
+        V_func, 
+        V̇_func, 
+        0.0, 
+        (0.0, 0.0),
+        nothing
+        )
+end
+
+for experiment in [:true_dynamics, :approx_no_data, :approx_with_data]
+    V_func = results[experiment].V
+    V̇_func = results[experiment].V̇
+
+    ############################## Simulate ###############################
+    V_predict = [V_func([x]) for x in simulation_xs]
+    dVdt_predict  = [V̇_func([x]) for x in simulation_xs]
+
+    # Print statistics
+    println("V(0.,0.) = ", V_func([0.0]))
+    println("dVdt(0.,0.) = ", V̇_func([0.0]))
+    println(
+        "V ∋ [", 
+        min(V_func([0.0]), 
+        minimum(V_predict)), 
+        ", ", 
+        maximum(V_predict), "]"
+    )
+    println(
+        "V̇ ∋ [",
+        minimum(dVdt_predict),
+        ", ",
+        max(V̇_func([0.0]), maximum(dVdt_predict)),
+        "]",
     )
 
-################################## Simulate ###################################
-xs = lb[1]:0.02:ub[1] 
-V_predict = [V_func([x0]) for x0 in xs]
-dVdt_predict  = [V̇_func([x0]) for x0 in xs]
+    # Get RoA Estimate
+    invalid_region = simulation_xs[dVdt_predict .>= 0]
+    invalid_start = maximum(invalid_region[invalid_region .< 0])
+    invalid_end = minimum(invalid_region[invalid_region .> 0])
+    valid_region = simulation_xs[invalid_start .< simulation_xs .< invalid_end]
+    ρ = min(V_func([first(valid_region)]), V_func([last(valid_region)]))
+    RoA = valid_region[vec(V_func(transpose(valid_region))) .≤ ρ]
 
-# Print statistics
-println("V(0.,0.) = ", V_func([0.0]))
-println("dVdt(0.,0.) = ", V̇_func([0.0]))
-println("V ∋ [", min(V_func([0.0]), minimum(V_predict)), ", ", maximum(V_predict), "]")
-println(
-    "V̇ ∋ [",
-    minimum(dVdt_predict),
-    ", ",
-    max(V̇_func([0.0]), maximum(dVdt_predict)),
-    "]",
-)
+    # Plot results
+    p_V = plot(simulation_xs, V_predict, label = "V", xlabel = "x", linewidth=2);
+    p_V = hline!([ρ], label = "V = $(round(ρ, digits = 4))", legend = :top);
+    p_V = vspan!([first(RoA), last(RoA)]; label = "Estimated Region of Attraction", opacity = 0.2, color = :green);
+    p_V = vspan!([-1, 1]; label = "True Region of Attraction", color = :gray, fillstyle = :/);
 
-# Get RoA Estimate
-invalid_region = xs[dVdt_predict .>= 0]
-invalid_start = maximum(invalid_region[invalid_region .< 0])
-invalid_end = minimum(invalid_region[invalid_region .> 0])
-valid_region = xs[invalid_start .< xs .< invalid_end]
-ρ = min(V_func([first(valid_region)]), V_func([last(valid_region)]))
-RoA = valid_region[vec(V_func(transpose(valid_region))) .≤ ρ]
+    p_V̇ = plot(simulation_xs, dVdt_predict, label = "dV/dt", xlabel = "x", linewidth=2);
+    p_V̇ = hline!([0.0], label = "dV/dt = 0", legend = :top);
+    p_V̇ = vspan!([first(RoA), last(RoA)]; label = "Estimated Region of Attraction", opacity = 0.2, color = :green);
+    p_V̇ = vspan!([-1, 1]; label = "True Region of Attraction", color = :gray, fillstyle = :/);
 
-println("Estimated region of attraction: [$(first(RoA)), $(last(RoA))]")
-println("True region of attraction: (-1, 1)")
+    plt_title = if experiment == :true_dynamics
+        "Using True Dynamics"
+    elseif experiment == :approx_no_data
+        "Using Approximate Dynamics Only"
+    else
+        "Using Approximate Dynamics + Data"
+    end
+        
+    plt = plot(p_V, p_V̇, plot_title=plt_title)
 
-# Plot results
-p1 = plot(xs, V_predict, label = "V", xlabel = "x", linewidth=2);
-p1 = hline!([ρ], label = "V = $(round(ρ, digits = 4))", legend = :inside)
-p1 = vspan!([first(RoA), last(RoA)]; label = "Estimated Region of Attraction", opacity = 0.2, color = :green);
-p1 = vspan!([-1, 1]; label = "True Region of Attraction", opacity = 0.2, color = :gray);
+    results[experiment] = LyapunovResults(
+        results[experiment].θ, 
+        V_func, 
+        V̇_func, 
+        ρ, 
+        (first(RoA), last(RoA)),
+        plt
+        )
+end
 
-p2 = plot(xs, dVdt_predict, label = "dV/dt", xlabel = "x", linewidth=2);
-p2 = hline!([0.0], label = "dV/dt = 0", legend = :bottom)
-p2 = vspan!([first(RoA), last(RoA)]; label = "Estimated Region of Attraction", opacity = 0.2, color = :green);
-p2 = vspan!([-1, 1]; label = "True Region of Attraction", opacity = 0.2, color = :gray);
+println("\n\nSummary\n")
+println("True region of attraction: (-1.0, 1.0)")
+println("RoA estimate with true dynamics: ", results[:true_dynamics].RoA)
+println("RoA estimate with approximate dynamics alone: ", results[:approx_no_data].RoA)
+println("RoA estimate with approximate dynamics + data: ", results[:approx_with_data].RoA)
 
-plot(p1, p2)
+plot(results[:true_dynamics].plt)
+plot(results[:approx_no_data].plt)
+plot(results[:approx_with_data].plt)
