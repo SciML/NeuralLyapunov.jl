4f0fef1cff48f31298648b3f806dec3ac810aeb1
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2024-03-29 15:29:40 -0400

    Tweak `local_lyapunov` tests to improve code coverage

--- test/local_lyapunov.jl
+++ test/local_lyapunov.jl
@@ -12,16 +12,21 @@ _V1, _V̇1 = local_lyapunov(f1, 3, CSDP.Optimizer, J1)
 V1(x) = _V1(x) ./ _V1([0, 0, 1])
 
 # Test equality
-xs = -1:0.1:1
-states = Iterators.map(collect, Iterators.product(xs, xs, xs))
-errs1 = @. abs(V1(states) - states ⋅ states)
+xs1 = -1:0.1:1
+states1 = Iterators.map(collect, Iterators.product(xs1, xs1, xs1))
+errs1 = @. abs(V1(states1) - states1 ⋅ states1)
 @test all(errs1 .< 1e-10)
 
 ########################## Simple harmonic oscillator #########################
-function f2(state, p, t)
+function f2(state::AbstractVector, p, t)
     ζ, ω_0 = p
     pos, vel = state
     vcat(vel, -2ζ * vel - ω_0^2 * pos)
+end
+function f2(states::AbstractMatrix, p, t)
+    ζ, ω_0 = p
+    pos, vel = states[1,:], states[2,:]
+    vcat(transpose(vel), transpose(-2ζ * vel - ω_0^2 * pos))
 end
 p2 = [3.2, 5.1]
 
@@ -30,11 +35,11 @@ V2, V̇2 = local_lyapunov(f2, 2, CSDP.Optimizer; p = p2)
 dV2dt = (state) -> ForwardDiff.derivative((δt) -> V2(state + δt * f2(state, p2, 0.0)), 0.0)
 
 # Test V̇2 = d/dt V2
-xs = -1:0.03:1
-states = Iterators.map(collect, Iterators.product(xs, xs))
-errs2 = @. abs(dV2dt(states) - V̇2(states))
+xs2 = -1:0.03:1
+states2 = hcat(Iterators.map(collect, Iterators.product(xs2, xs2))...)
+errs2 = abs.( V̇2(states2) .- dV2dt(states2))
 @test all(errs2 .< 1e-10)
 
 # Test V̇2 ≺ 0
-@test all( @. V̇2(states) < 0)
+@test all( V̇2(states2) .< 0)
 @test V̇2(zeros(2)) == 0.0
