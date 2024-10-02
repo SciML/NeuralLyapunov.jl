47dc6a9d2c974b666d13d09b26d0622372f17789
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2024-10-02 15:22:15 -0400

    Added `ODESystem` benchmark support and verbose option for `benchmark`

--- src/benchmark_harness.jl
+++ src/benchmark_harness.jl
@@ -1,6 +1,3 @@
-# using NeuralPDE, Optimization, DifferentialEquations, EvalMetrics
-
-#=
 function benchmark(
     dynamics::ODESystem,
     bounds,
@@ -8,9 +5,16 @@ function benchmark(
     chain,
     strategy,
     opt;
+    simulation_time,
+    n_grid,
+    classifier = (V, V̇, x) -> V̇ < zero(V̇),
     fixed_point = zeros(length(bounds)),
-    p = SciMLBase.NullParameters(),
-    optimzation_args = []
+    optimization_args = [],
+    ode_solver = Tsit5(),
+    ode_solver_args = [],
+    atol = 1e-6,
+    endpoint_check = (x) -> ≈(x, fixed_point; atol=atol),
+    verbose = false
 )
     @named pde_system = NeuralLyapunovPDESystem(
         dynamics,
@@ -19,24 +23,41 @@ function benchmark(
         fixed_point = fixed_point
     )
 
-    θ, net = _benchmark_solve(
+    f, params = if isempty(ModelingToolkit.unbound_inputs(dynamics))
+        ODEFunction(dynamics), parameters(dynamics)
+    else
+        (_f, _), _, _p = ModelingToolkit.generate_control_function(dynamics, simplify = true)
+        _f, _p
+    end
+
+    defaults = ModelingToolkit.get_defaults(dynamics)
+    p = [defaults[param] for param in params]
+
+    lb = [d.domain.left for d in bounds]
+    ub = [d.domain.right for d in bounds]
+
+    return _benchmark(
         pde_system,
+        f,
+        lb,
+        ub,
+        spec,
         chain,
         strategy,
         opt;
-        optimization_args = optimization_args
-    )
-
-    V, V̇ = get_numerical_lyapunov_function(
-        net,
-        θ,
-        spec.structure,
-        dynamics,
-        fixed_point;
-        p = p
+        simulation_time,
+        n_grid,
+        classifier,
+        fixed_point,
+        p,
+        optimization_args,
+        ode_solver,
+        ode_solver_args,
+        atol,
+        endpoint_check,
+        verbose
     )
 end
-=#
 
 function benchmark(
     dynamics::Function,
@@ -57,8 +78,11 @@ function benchmark(
     optimization_args = [],
     ode_solver = Tsit5(),
     ode_solver_args = [],
-    atol = 1e-6
+    atol = 1e-6,
+    endpoint_check = (x) -> ≈(x, fixed_point; atol=atol),
+    verbose = false
 )
+    # Build PDESystem
     @named pde_system = NeuralLyapunovPDESystem(
         dynamics,
         lb,
@@ -71,6 +95,50 @@ function benchmark(
         policy_search = policy_search
     )
 
+    return _benchmark(
+        pde_system,
+        dynamics,
+        lb,
+        ub,
+        spec,
+        chain,
+        strategy,
+        opt;
+        simulation_time,
+        n_grid,
+        classifier,
+        fixed_point,
+        p,
+        optimization_args,
+        ode_solver,
+        ode_solver_args,
+        atol,
+        endpoint_check,
+        verbose
+    )
+end
+
+function _benchmark(
+    pde_system,
+    f,
+    lb,
+    ub,
+    spec,
+    chain,
+    strategy,
+    opt;
+    simulation_time,
+    n_grid,
+    classifier,
+    fixed_point,
+    p,
+    optimization_args,
+    ode_solver,
+    ode_solver_args,
+    atol,
+    endpoint_check,
+    verbose
+)
     t = @timed benchmark_solve(
         pde_system,
         chain,
@@ -85,36 +153,53 @@ function benchmark(
         phi,
         θ,
         spec.structure,
-        dynamics,
+        f,
         fixed_point;
         p = p
     )
 
-    f = let fc = spec.structure.f_call, _dynamics = dynamics,
+    f = let fc = spec.structure.f_call, _f = f,
             net = NeuralLyapunov.phi_to_net(phi, θ)
-            ODEFunction(
-                (x, _p, t) -> fc(_dynamics, net, x, _p, t),
-                sys = SciMLBase.SymbolCache(state_syms, parameter_syms)
-            )
+            ODEFunction((x, _p, t) -> fc(_f, net, x, _p, t))
         end
 
     states, V_samples, V̇_samples = eval_Lyapunov(lb, ub, n_grid, V, V̇)
 
-    cm = build_confusion_matrix(
-        collect(states),
-        V_samples,
-        V̇_samples,
-        f;
-        classifier,
-        fixed_point,
-        simulation_time,
-        p,
-        ode_solver,
-        ode_solver_args,
-        atol
-    )
-
-    return (cm, solve_time)
+    if verbose
+        cm, states, endpoints, actual, predicted, V_samples, V̇_samples = build_confusion_matrix(
+            collect(states),
+            V_samples,
+            V̇_samples,
+            f;
+            classifier,
+            fixed_point,
+            simulation_time,
+            p,
+            ode_solver,
+            ode_solver_args,
+            atol,
+            endpoint_check,
+            verbose
+        )
+        return ((cm, solve_time), (states, endpoints, actual, predicted, V_samples, V̇_samples))
+    else
+        cm = build_confusion_matrix(
+            collect(states),
+            V_samples,
+            V̇_samples,
+            f;
+            classifier,
+            fixed_point,
+            simulation_time,
+            p,
+            ode_solver,
+            ode_solver_args,
+            atol,
+            endpoint_check,
+            verbose
+        )
+        return (cm, solve_time)
+    end
 end
 
 function benchmark_solve(pde_system, chain, strategy, opt; optimization_args)
@@ -165,7 +250,9 @@ function build_confusion_matrix(
     p,
     ode_solver,
     ode_solver_args,
-    atol
+    atol,
+    endpoint_check,
+    verbose
 )
     predicted = classifier.(V_samples, V̇_samples, states)
     ix0 = findfirst(x -> x == fixed_point, states)
@@ -173,6 +260,19 @@ function build_confusion_matrix(
         predicted[ix0] = true
     end
 
+    endpoints = [
+        get_endpoint(
+            dynamics,
+            x,
+            simulation_time;
+            p = p,
+            solver = ode_solver,
+            solver_args = ode_solver_args
+        ) for x in states
+    ]
+
+    actual = endpoint_check.(endpoints)
+#=
     actual = [
         ≈(
             get_endpoint(
@@ -187,6 +287,13 @@ function build_confusion_matrix(
             atol = atol
         ) for x in states
     ]
+=#
 
     cm = ConfusionMatrix(vec(actual), vec(predicted))
+
+    if verbose
+        (cm, states, endpoints, actual, predicted, V_samples, V̇_samples)
+    else
+        cm
+    end
 end
--- test/benchmark.jl
+++ test/benchmark.jl
@@ -4,6 +4,7 @@ using Random
 using Test
 
 ###################### Damped simple harmonic oscillator ######################
+@testset "Simple harmonic oscillator benchmarking" begin
 
 println("Benchmark: Damped SHO")
 Random.seed!(200)
@@ -73,15 +74,16 @@ cm, time = benchmark(
     optimization_args = optimization_args
 )
 
-@testset "Simple Harmonic Oscillator Benchmarking" begin
-    # SHO is globally asymptotically stable
-    @test cm.n == 0
+# SHO is globally asymptotically stable
+@test cm.n == 0
 
-    # Should accurately classify
-    @test cm.fn / cm.p < 0.5
+# Should accurately classify
+@test cm.fn / cm.p < 0.5
+
 end
 
 ####################### Inverted pendulum policy search #######################
+@testset "Policy search on inverted pendulum benchmarking" begin
 
 println("Benchmark: Inverted Pendulum - Policy Search")
 Random.seed!(200)
@@ -167,10 +169,103 @@ cm, time = benchmark(
     atol = 5e-3
 )
 
-@testset "Policy search on inverted pendulum" begin
-    # Resulting controller should drive more states to equilibrium than not
-    @test cm.p > cm.n broken=true
+# Resulting controller should drive more states to equilibrium than not
+@test cm.p > cm.n broken=true
 
-    # Resulting classifier should be accurate
-    @test (cm.tp + cm.tn) / (cm.p + cm.n) > 0.9 broken=true
+# Resulting classifier should be accurate
+@test (cm.tp + cm.tn) / (cm.p + cm.n) > 0.9 broken=true
+
+end
+
+############################### Damped pendulum ###############################
+@testset "Damped pendulum benchmarking" begin
+
+println("Benchmark: Damped Pendulum")
+Random.seed!(200)
+
+# Define dynamics and domain
+@parameters ζ ω_0
+defaults = Dict([ζ => 5.0, ω_0 => 1.0])
+
+@variables t θ(t)
+Dt = Differential(t)
+DDt = Dt^2
+
+eqs = [DDt(θ) + 2ζ * ω_0 * Dt(θ) + ω_0^2 * sin(θ) ~ 0.0]
+
+@named damped_pendulum = ODESystem(
+    eqs,
+    t,
+    [θ],
+    [ζ, ω_0];
+    defaults = defaults
+)
+
+damped_pendulum = structural_simplify(damped_pendulum)
+bounds = [
+    θ ∈ (-π, π),
+    Dt(θ) ∈ (-10.0, 10.0)
+]
+
+# Define neural network discretization
+# We use an input layer that is periodic with period 2π with respect to θ
+dim_state = length(bounds)
+dim_hidden = 15
+dim_output = 2
+chain = [Lux.Chain(
+             Boltz.Layers.PeriodicEmbedding([1], [2π]),
+             Dense(3, dim_hidden, tanh),
+             Dense(dim_hidden, dim_hidden, tanh),
+             Dense(dim_hidden, 1, use_bias = false)
+         ) for _ in 1:dim_output]
+
+# Define neural network discretization
+strategy = GridTraining(0.1)
+
+# Define neural Lyapunov structure
+structure = PositiveSemiDefiniteStructure(
+    dim_output;
+    pos_def = function (state, fixed_point)
+        θ, ω = state
+        θ_eq, ω_eq = fixed_point
+        log(1.0 + (sin(θ) - sin(θ_eq))^2 + (cos(θ) - cos(θ_eq))^2 + (ω - ω_eq)^2)
+    end
+)
+minimization_condition = DontCheckNonnegativity(check_fixed_point = false)
+
+# Define Lyapunov decrease condition
+decrease_condition = AsymptoticStability()
+
+# Construct neural Lyapunov specification
+spec = NeuralLyapunovSpecification(
+    structure,
+    minimization_condition,
+    decrease_condition
+)
+
+# Define optimization parameters
+opt = OptimizationOptimisers.Adam()
+optimization_args = [:maxiters => 600]
+
+
+# Run benchmark
+cm, time = benchmark(
+    damped_pendulum,
+    bounds,
+    spec,
+    chain,
+    strategy,
+    opt;
+    simulation_time = 300,
+    n_grid = 20,
+    optimization_args = optimization_args,
+    endpoint_check = (x) -> ≈([sin(x[1]), cos(x[1]), x[2]], [0, 1, 0], atol=1e-3)
+)
+
+# Damped pendulum is globally asymptotically stable, except at upright equilibrium
+@test cm.n == 2
+
+# Should accurately classify
+@test cm.fn / cm.p < 0.5
+
 end
