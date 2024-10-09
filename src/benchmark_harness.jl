92f5e1fea97273b3b6fc248ed398af2786968d12
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2024-10-09 15:21:26 -0400

    Added documentation for `benchmark`

--- src/benchmark_harness.jl
+++ src/benchmark_harness.jl
@@ -1,3 +1,84 @@
+"""
+    benchmark(dynamics, bounds, spec, chain, strategy, opt; <keyword_arguments>)
+    benchmark(dynamics, lb, ub, spec, chain, strategy, opt; <keyword_arguments>)
+
+Evaluate the specified neural Lyapunov method on the given system. Return the confusion
+matrix and optimization time.
+
+Train a neural Lyapunov function as specified, then discretize the domain using a grid
+discretization and use the neural Lyapnov function to and the provided `classifier` to
+predict whether grid points are in the region of attraction of the provided `fixed_point`.
+Finally, simulate the system from each grid point and check if the trajectories reach the
+fixed point. Return a confusion matrix for the neural Lyapunov classifier using the results
+of the simulated trajectories as ground truth. Additionally return the time it took for the
+optimization to run.
+
+# Positional Arguments
+  - `dynamics`: the dynamical system being analyzed, represented as an `ODESystem` or the
+    function `f` such that `ẋ = f(x[, u], p, t)`; either way, the ODE should not depend on
+    time and only `t = 0.0` will be used. (For an example of when `f` would have a `u`
+    argument, see [`add_policy_search`](@ref).)
+  - `bounds`: an array of domains, defining the training domain by bounding the states (and
+    derivatives, when applicable) of `dynamics`; only used when `dynamics isa
+    ODESystem`, otherwise use `lb` and `ub`.
+  - `lb` and `ub`: the training domain will be ``[lb_1, ub_1]×[lb_2, ub_2]×...``; not used
+    when `dynamics isa ODESystem`, then use `bounds`.
+  - `spec`: a [`NeuralLyapunovSpecification`](@ref) defining the Lyapunov function
+    structure, as well as the minimization and decrease conditions.
+  - `chain`: a vector of Lux/Flux chains with a d-dimensional input and a 1-dimensional
+    output corresponding to each of the dependent variables, where d is the length of
+    `bounds` or `lb` and `ub`. Note that this specification respects the order of the
+    dependent variables as specified in the PDESystem. Flux chains will be converted to Lux
+    internally by NeuralPDE using `NeuralPDE.adapt(FromFluxAdaptor(false, false), chain)`.
+  - `strategy`: determines which training strategy will be used. See the NeuralPDE Training
+    Strategy documentation for more details.
+  - `opt`: optimizer to use in training the neural Lyapunov function.
+
+# Keyword Arguments
+  - `n_grid`: number or grid points in each dimension used for evaluating the neural
+    Lyapunov classifier.
+  - `classifier`: function of ``V(x)``, ``V̇(x)``, and ``x`` that predicts whether ``x`` is
+    in the region of attraction.
+  - `fixed_point`: the equilibrium being analyzed; defaults to the origin.
+  - `p`: the values of the parameters of the dynamical system being analyzed; defaults to
+    `SciMLBase.NullParameters()`; not used when `dynamics isa ODESystem`, then use the
+    default parameter values of `dynamics`.
+  - `state_syms`: an array of the `Symbol` representing each state; not used when `dynamics
+    isa ODESystem`, then the symbols from `dynamics` are used; if `dynamics isa ODEFunction`,
+    symbols stored there are used, unless overridden here; if not provided here and cannot
+    be inferred, `[:state1, :state2, ...]` will be used.
+  - `parameter_syms`: an array of the `Symbol` representing each parameter; not used when
+    `dynamics isa ODESystem`, then the symbols from `dynamics` are used; if `dynamics isa
+    ODEFunction`, symbols stored there are used, unless overridden here; if not provided
+    here and cannot be inferred, `[:param1, :param2, ...]` will be used.
+  - `policy_search::Bool`: whether or not to include a loss term enforcing `fixed_point` to
+    actually be a fixed point; defaults to `false`; only used when `dynamics isa Function &&
+    !(dynamics isa ODEFunction)`; when `dynamics isa ODEFunction`, `policy_search` should
+    not be supplied (as it must be false); when `dynamics isa ODESystem`, value inferred by
+    the presence of unbound inputs.
+  - `optimization_args`: arguments to be passed into the optimization solver. For more
+    information, see the
+    [Optimization.jl docs](https://docs.sciml.ai/Optimization/stable/API/solve/).
+  - `simulation_time`: simulation end time for checking if trajectory from a point reaches
+    equilibrium
+  - `ode_solver`: differential equation solver used in simulating the system for evaluation.
+    For more information, see the
+    [DifferentialEquations.jl docs](https://docs.sciml.ai/DiffEqDocs/stable/solvers/ode_solve/).
+  - `ode_solver_args`: arguments to be passed into the differential equation solver. For
+    more information, see the
+    [DifferentialEquations.jl docs](https://docs.sciml.ai/DiffEqDocs/stable/basics/common_solver_opts/).
+  - `endpoint_check`: function of the endpoint of a simulation that returns `true` when the
+    endpoint is approximately the fixed point and `false` otherwise; defaults to
+    `(x) -> ≈(x, fixed_point; atol=atol)`.
+  - `atol`: absolute tolerance used in the default value for `endpoint_check`.
+  - `verbose`: enable verbose output. Instead of outputting
+    `(confusion_matrix, training_time)`, output
+    `((confusion_matrix, training_time), (states, endpoints, actual, predicted, V_samples, V̇_samples))`,
+    where `states` is the grid of evaluation points, `endpoints` is the endpoints of the
+    simulations, `actual` is the result of `endpoint_check` applied to `endpoints`,
+    `predicted` is the result of `classifier` applied to `states`, `V_samples` is ``V``
+    evaluated at `states`, and `V̇_samples` is ``V̇`` evaluated at `states`.
+"""
 function benchmark(
     dynamics::ODESystem,
     bounds,
@@ -5,11 +86,11 @@ function benchmark(
     chain,
     strategy,
     opt;
-    simulation_time,
     n_grid,
     classifier = (V, V̇, x) -> V̇ < zero(V̇),
     fixed_point = zeros(length(bounds)),
     optimization_args = [],
+    simulation_time,
     ode_solver = Tsit5(),
     ode_solver_args = [],
     atol = 1e-6,
@@ -45,12 +126,12 @@ function benchmark(
         chain,
         strategy,
         opt;
-        simulation_time,
         n_grid,
         classifier,
         fixed_point,
         p,
         optimization_args,
+        simulation_time,
         ode_solver,
         ode_solver_args,
         endpoint_check,
@@ -66,7 +147,6 @@ function benchmark(
     chain,
     strategy,
     opt;
-    simulation_time,
     n_grid,
     classifier = (V, V̇, x) -> V̇ < zero(V̇),
     fixed_point = zeros(length(lb)),
@@ -75,6 +155,7 @@ function benchmark(
     parameter_syms = [],
     policy_search = false,
     optimization_args = [],
+    simulation_time,
     ode_solver = Tsit5(),
     ode_solver_args = [],
     atol = 1e-6,
@@ -103,12 +184,12 @@ function benchmark(
         chain,
         strategy,
         opt;
-        simulation_time,
         n_grid,
         classifier,
         fixed_point,
         p,
         optimization_args,
+        simulation_time,
         ode_solver,
         ode_solver_args,
         endpoint_check,
@@ -125,12 +206,12 @@ function _benchmark(
     chain,
     strategy,
     opt;
-    simulation_time,
     n_grid,
     classifier,
     fixed_point,
     p,
     optimization_args,
+    simulation_time,
     ode_solver,
     ode_solver_args,
     endpoint_check,
