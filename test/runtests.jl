1f459fe48daecb2b2ea5c126df716365369b2935
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2025-05-05 16:05:18 -0400

    Moves GPU test from its own test group to core and adds "DEVICE" to ENV to distinguish between GPU and CPU

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,59 +1,74 @@
 using SafeTestsets
 
 const GROUP = lowercase(get(ENV, "GROUP", "all"))
+const DEVICE = lowercase(get(ENV, "DEVICE", "cpu"))
 
 @time begin
     if GROUP == "all" || GROUP == "core"
-        @time @safetestset "Damped simple harmonic oscillator" begin
-            include("damped_sho.jl")
+        if DEVICE == "cpu"
+            @time @safetestset "Damped simple harmonic oscillator" begin
+                include("damped_sho.jl")
+            end
+            @time @safetestset "Damped pendulum" begin
+                include("damped_pendulum.jl")
+            end
         end
-        @time @safetestset "Damped pendulum" begin
-            include("damped_pendulum.jl")
+
+        if DEVICE == "gpu"
+            @time @safetestset "CUDA test - Damped SHO" begin
+                include("damped_sho_CUDA.jl")
+            end
         end
     end
 
     if GROUP == "all" || GROUP == "policy_search"
-        @time @safetestset "Policy search - inverted pendulum" begin
-            include("inverted_pendulum.jl")
-        end
-        @time @safetestset "Policy search - inverted pendulum (ODESystem)" begin
-            include("inverted_pendulum_ODESystem.jl")
+        if DEVICE == "cpu"
+            @time @safetestset "Policy search - inverted pendulum" begin
+                include("inverted_pendulum.jl")
+            end
+            @time @safetestset "Policy search - inverted pendulum (ODESystem)" begin
+                include("inverted_pendulum_ODESystem.jl")
+            end
         end
     end
 
     if GROUP == "all" || GROUP == "roa"
-        @time @safetestset "Region of attraction estimation" begin
-            include("roa_estimation.jl")
+        if DEVICE == "cpu"
+            @time @safetestset "Region of attraction estimation" begin
+                include("roa_estimation.jl")
+            end
         end
     end
 
     if GROUP == "all" || GROUP == "local_lyapunov"
-        @time @safetestset "Local Lyapunov function search" begin
-            include("local_lyapunov.jl")
-        end
-    end
-
-    if GROUP == "gpu"
-        @time @safetestset "CUDA test - Damped SHO" begin
-            include("damped_sho_CUDA.jl")
+        if DEVICE == "cpu"
+            @time @safetestset "Local Lyapunov function search" begin
+                include("local_lyapunov.jl")
+            end
         end
     end
 
     if GROUP == "all" || GROUP == "benchmarking"
-        @time @safetestset "Benchmarking tool" begin
-            include("benchmark.jl")
+        if DEVICE == "cpu"
+            @time @safetestset "Benchmarking tool" begin
+                include("benchmark.jl")
+            end
         end
     end
 
     if GROUP == "all" || GROUP == "unimplemented"
-        @time @safetestset "Errors for partially-implemented extensions" begin
-            include("unimplemented.jl")
+        if DEVICE == "cpu"
+            @time @safetestset "Errors for partially-implemented extensions" begin
+                include("unimplemented.jl")
+            end
         end
     end
 
     if GROUP == "all" || GROUP == "qa"
-        @time @safetestset "Quality Assurance" begin
-            include("qa_tests.jl")
+        if DEVICE == "cpu"
+                @time @safetestset "Quality Assurance" begin
+                include("qa_tests.jl")
+            end
         end
     end
 end
