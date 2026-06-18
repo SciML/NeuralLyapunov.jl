e5af87ec478eea1c8b939746183d1c85c82c9a60
Nicholas Klugman <13633349+nicholaskl97@users.noreply.github.com>
2026-06-18 15:59:09 -0400

    Fix Julia v1.10 explicit import tests for use of non-public imports/accesses that are not exported but are `public`

--- test/QA/explicit_imports.jl
+++ test/QA/explicit_imports.jl
@@ -9,20 +9,32 @@ import NeuralLyapunov
     @test check_all_explicit_imports_via_owners(NeuralLyapunov) === nothing
     @test check_all_qualified_accesses_via_owners(NeuralLyapunov) === nothing
 
-    # We need a Symbolics internal (diff2term), a ModelingToolkit internal (unbound_inputs),
-    # and for some reason QuasiMonteCarlo doesn't export sample
-    @test check_all_explicit_imports_are_public(
-        NeuralLyapunov; ignore = (:diff2term, :sample, :unbound_inputs)
-    ) === nothing
+    # We need a Symbolics internal (diff2term) and for some reason QuasiMonteCarlo doesn't
+    # export sample or mark it as public
+    if VERSION >= v"1.11.0-DEV.469"
+        ignore = (:diff2term, :sample)
+    else
+        # ShiftTo, checksquare, and unbound_inputs are public in Julia v1.11+, but not
+        # exported in Julia v1.10
+        ignore = (:diff2term, :sample, :ShiftTo, :checksquare, :unbound_inputs)
+    end
+    @test check_all_explicit_imports_are_public(NeuralLyapunov; ignore) === nothing
 
     # ForwardDiff doesn't export derivative, gradient, or jacobian, nor does SciMLBase
     # export NullParameters, __has_jac, or __has_control_jac, nor does Symbolics export
     # value
-    @test check_all_qualified_accesses_are_public(
-        NeuralLyapunov;
+    if VERSION >= v"1.11.0-DEV.469"
         ignore = (
             :NullParameters, :derivative, :gradient, :jacobian, :logscalar, :__has_jac,
             :__has_controljac, :value,
         )
-    ) === nothing
+    else
+        # Fix1, Fix2, initialparameters, and initialstates are public in Julia v1.11+, but
+        # not exported in Julia v1.10
+        ignore = (
+            :NullParameters, :derivative, :gradient, :jacobian, :logscalar, :__has_jac,
+            :__has_controljac, :value, :Fix1, :Fix2, :initialparameters, :initialstates,
+        )
+    end
+    @test check_all_qualified_accesses_are_public(NeuralLyapunov; ignore) === nothing
 end
